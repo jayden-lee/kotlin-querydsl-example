852e1f05217b1d4081bc260a8cf52e59d7980d86
jayden-lee <gwanggi.lee@gmail.com>
2020-02-22 15:27:24 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/jayden/kotlin/querydsl/QuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.jayden.kotlin.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuerydslApplication>(*args)
+}
